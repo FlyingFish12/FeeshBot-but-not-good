d740aa63b635169c0ffd5543ea82bd9b4075755e
rpxs <66899904+rpxs@users.noreply.github.com>
2020-12-24 11:30:06 -0700

    Formatted with Prettier

--- commands/worldrecordfact.js
+++ commands/worldrecordfact.js
@@ -1,19 +1,25 @@
-const { MessageEmbed } = require('discord.js');
-const Discord = require('discord.js');
-const human = require('humanize');
-const paginationEmbed = require('discord.js-pagination');
-module.exports ={
-    name: 'worldrecordfact',
-    description: "An worldrecord fact command",
-    aliases: ['record', 'worldrecord','recordfact', 'recordfacts', 'worldrecordfacts', 'rf'],
-    execute(message,args,client){
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
+const human = require("humanize");
+const paginationEmbed = require("discord.js-pagination");
+module.exports = {
+  name: "worldrecordfact",
+  description: "An worldrecord fact command",
+  aliases: [
+    "record",
+    "worldrecord",
+    "recordfact",
+    "recordfacts",
+    "worldrecordfacts",
+    "rf",
+  ],
+  execute(message, args, client) {
+    const jsonData = require("../arrays/rcd.json");
 
-        const jsonData = require('../arrays/rcd.json')
+    const records = Object.values(jsonData);
 
-const records = Object.values(jsonData)
+    const randomRecord = records[parseInt(Math.random() * records.length)];
 
-const randomRecord = records[parseInt(Math.random() * records.length)]
-
-message.channel.send(randomRecord)
-    }
-}
+    message.channel.send(randomRecord);
+  },
+};
