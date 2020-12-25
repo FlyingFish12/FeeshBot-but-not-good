f202f09289afe2fd9f95c9bc3de72357fef4153c
rpxs <me@rpxs.ml>
2020-12-24 21:13:36 -0700

    Formatted with Prettier

--- commands/topic.js
+++ commands/topic.js
@@ -1,19 +1,14 @@
-const { MessageEmbed } = require('discord.js');
-const Discord = require('discord.js');
-const human = require('humanize');
-const paginationEmbed = require('discord.js-pagination');
-module.exports ={
-    name: 'topic',
-    description: "A topic command",
-    aliases: ['tpc'],
-    execute(message,args,client){
-
-const jsonData = require('../arrays/topics.json')
-
-const topics = Object.values(jsonData)
-
-const randomTopic = topics[parseInt(Math.random() * topics.length)]
-
-message.channel.send(randomTopic + ` - DEBATE!`);
-    }
-}
+const { MessageEmbed, Discord } = require("discord.js");
+const human = require("humanize");
+const paginationEmbed = require("discord.js-pagination");
+module.exports = {
+  name: "topic",
+  description: "A topic command",
+  aliases: ["tpc"],
+  execute(message, args, client) {
+    const jsonData = require("../arrays/topics.json");
+    const topics = Object.values(jsonData);
+    const randomTopic = topics[parseInt(Math.random() * topics.length)];
+    message.channel.send(randomTopic + ` - DEBATE!`);
+  },
+};
