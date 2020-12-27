280d8ea77003ee9720b69ffb465d27845e610182
Fefe <F33f11f00fum@gmail.com>
2020-12-27 16:08:59 +0000

    added a say command, idk if it works

--- commands/say.js
+++ commands/say.js
@@ -6,14 +6,13 @@ module.exports ={
     name: 'say',
     description: "A Say Command",
     aliases: ['say','s','write'],
-    execute(message,client){
+    execute(message,args,client){
 
-    let args = message.content.split(" ").slice(1);
     
     message.delete();
         const sayembed = new Discord.MessageEmbed()
             .setColor('#50C878')
-            .setDescription(message.args.join(" ")).cleanContent;
+            .setDescription(message.args.join(` `)).cleanContent;
 
     }
 }
