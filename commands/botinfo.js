e3e6422ae103476be930eff798ef2469ce20f702
Fefe <F33f11f00fum@gmail.com>
2021-01-04 16:46:22 +0000

    fixed it a bit

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -8,6 +8,8 @@ module.exports ={
     aliases: ['bi', 'info'],
     execute(message,args,client){
 
+     const guilds = client.guilds.cache.array()
+
        let days = Math.floor(client.uptime / 86400000);
        let hours = Math.floor(client.uptime / 3600000) % 24;
        let minutes = Math.floor(client.uptime / 60000) % 60;
