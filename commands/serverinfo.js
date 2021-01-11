97064b8e54703baf50a44500dc2e58496943659c
Fefe <F33f11f00fum@gmail.com>
2021-01-11 10:59:58 +0000

    Made the server info command have pretty emotes :D

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,22 +7,31 @@ module.exports ={
     description: "A serverinfo command",
     aliases: ['server','svr','si','guild','guildinfo'],
     execute(message,args,client){
+        const Owner_Crown = client.emojis.cache.get('797172246132817972')
+        const Earth_Rotate = client.emojis.cache.get('797173733243748383')
+        const ID_Logo = client.emojis.cache.get('797186040430854215')
+        const Moderation = client.emojis.cache.get('798141615989719040')
+        const Bot_Member = client.emojis.cache.get('797177406573903892')
+        const Human_Member = client.emojis.cache.get('797528900992499732')
+        const All_Member = client.emojis.cache.get('798142246901514270')
+        const Server_Creation = client.emojis.cache.get('798142682362019840')
+        const Name = client.emojis.cache.get('798143819953864744')
         const guildOwner = message.guild.owner.user.tag
-       const humanCount = message.guild.members.cache.filter(member => !member.user.bot).size;
-       const botCount = message.guild.members.cache.filter(member => member.user.bot).size;
-       const serverEmbed = new Discord.MessageEmbed()
+        const humanCount = message.guild.members.cache.filter(member => !member.user.bot).size;
+        const botCount = message.guild.members.cache.filter(member => member.user.bot).size;
+        const serverEmbed = new Discord.MessageEmbed()
                .setColor(`#4893C0`)
                .setTitle(`Server Info`)
                .addFields(
-               {name: `Server Name`, value: `\`\`\`${message.guild.name}\`\`\``, inline: true},
-               {name: `Server Owner`, value: `\`\`\`${guildOwner}\`\`\``, inline: true},
-               {name: `Server ID`, value: `\`\`\`${message.guild.id}\`\`\``, inline: true},
-               {name: `Server Moderation Level`, value: `\`\`\`${message.guild.verificationLevel || 'None'}\`\`\``, inline: true},
-               {name: `Server Region`, value: `\`\`\`${message.guild.region}\`\`\``, inline: true},
-               {name: `Server Creation`, value: `\`\`\`${human.date('d.m.y | h:i:s', message.guild.createdAt)} ${message.guild.createdAt >= 12? "AM" : "PM"}\`\`\``, inline: true},
-               {name: `Server Members`, value: `\`\`\`${message.guild.memberCount}\`\`\``, inline: true},
-               {name: `Human Members`, value: `\`\`\`${humanCount}\`\`\``, inline: true},
-               {name: `Bot Members`, value: `\`\`\`${botCount}\`\`\``, inline: true})
+               {name: `Server Name ${Name}`, value: `\`\`\`${message.guild.name}\`\`\``, inline: true},
+               {name: `Server Owner ${Owner_Crown}`, value: `\`\`\`${guildOwner}\`\`\``, inline: true},
+               {name: `Server ID ${ID_Logo}`, value: `\`\`\`${message.guild.id}\`\`\``, inline: true},
+               {name: `Server Moderation Level ${Moderation}`, value: `\`\`\`${message.guild.verificationLevel || 'None'}\`\`\``, inline: true},
+               {name: `Server Region ${Earth_Rotate}`, value: `\`\`\`${message.guild.region}\`\`\``, inline: true},
+               {name: `Server Creation ${Server_Creation}`, value: `\`\`\`${human.date('d.m.y | h:i:s', message.guild.createdAt)} ${message.guild.createdAt >= 12? "AM" : "PM"}\`\`\``, inline: true},
+               {name: `Server Members ${All_Member}`, value: `\`\`\`${message.guild.memberCount}\`\`\``, inline: true},
+               {name: `Human Members ${Human_Member}`, value: `\`\`\`${humanCount}\`\`\``, inline: true},
+               {name: `Bot Members ${Bot_Member}`, value: `\`\`\`${botCount}\`\`\``, inline: true})
                .setTimestamp()
                .setFooter(`Say "F?ServerRoles" for a list of the servers roles!`)
        message.channel.send(serverEmbed);
