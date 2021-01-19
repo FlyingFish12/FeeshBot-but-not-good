a6bce3bb7b6de5396d9f911317a5da7055a224da
Fefe <F33f11f00fum@gmail.com>
2021-01-19 18:48:04 +0000

    changed vote command

--- commands/vote.js
+++ commands/vote.js
@@ -1,19 +1,39 @@
-const { MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
-const human = require('humanize');
-const paginationEmbed = require('discord.js-pagination');
-module.exports={
+const { MessageEmbed } = require('discord.js');
+const Client = new Discord.Client();
+const Topgg = require('@top-gg/sdk')
+const api = new Topgg.Api('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczNTkxODMxMzkyMTcwODA1MyIsImJvdCI6dHJ1ZSwiaWF0IjoxNjExMDgxNzI3fQ.tb0JNNGE6OivNEwPVP2hacoY4E4YoCK2_TBJBzxa2xs')
+module.exports = {
     name: 'vote',
-    description: "upvote or I cri (can fish cry?)",
-    aliases: ['botinvite'],
-    execute(message,args,client){
-        const InviteEmbed = new Discord.MessageEmbed()
-        .setColor(`#DA005A`)
-        .setTitle("Upvote FeeshBot Today!")
-        .setDescription("[Upvote FeeshBot on DBL](https://discord.ly/feesh)")
-        .setFooter("Vote Rewards Coming Soon!")
-        .setTimestamp()
-message.channel.send(InviteEmbed)                
+    description: "Get's the vote link",
+   async execute(message, args){
+    
+    let has_voted;
 
+    let votes = [];
+
+    let apiVotes = await api.getVotes();
+    for (let i = 0; i < 10; i++) {
+      votes.push(i+1 + " - " + apiVotes[i]["username"] + "#" + apiVotes[i]["discriminator"]);
     }
+
+    let canVote = await api.hasVoted(message.author.id)
+
+    if (canVote) { has_voted = `<a:check_yes:798896012773031938> You have voted for **[VibeMaster](https://top.gg/bot/735918313921708053)**!`; } else { has_voted = `<a:check_no:798895971265675294> You have not voted for **[VibeMaster](https://top.gg/bot/783629275546648577/vote)**!` }
+    let vote_for_feesh = `**[Click Me To Vote](https://top.gg/bot/735918313921708053)**`
+  const VoteEmbed = new Discord.MessageEmbed()
+    .setColor('#3586cd')
+    .setTitle('Vote for VibeMaster')
+    .addField(`Vote for me `, vote_for_feesh)
+    .setDescription("```css\n[ RECENT VOTES ]\n\n" + votes.join('\n') + "```")
+    .addField('Vote Status', has_voted);
+  message.channel.send(VoteEmbed);
+  const botlistspace = `**[botlist.space](https://discordbotlist.com/bots/feesh)**`
+  const OtherVotesEmbed = new Discord.MessageEmbed()
+  .setColor('#3586cd')
+  .setTitle('Vote for Feesh on these other sites')
+  .addField(`${botlistspace}`)
+  message.channel.send(OtherVotesEmbed)
+  
+  }
 }
