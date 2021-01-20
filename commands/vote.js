b176f5ba92d420858c401922232b5951f15d56bb
Fefe <F33f11f00fum@gmail.com>
2021-01-20 10:10:34 +0000

    added vote command again

---
+++ commands/vote.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+const human = require('humanize');
+const paginationEmbed = require('discord.js-pagination');
+module.exports={
+    name: 'vote',
+    description: "upvote or I cri (can fish cry?)",
+    aliases: ['votes', 'v'],
+    execute(message,args,client){
+        const voteEmbed = new Discord.MessageEmbed()
+        .setColor(`#DA005A`)
+        .setTitle("Upvote FeeshBot Today!")
+        .setDescription("[Upvote FeeshBot on DBL](https://discord.ly/feesh)\n[Upvote FeeshBot on Top.gg](https://top.gg/bot/735918313921708053)")
+        .setFooter("Vote Rewards Coming Soon!")
+        .setTimestamp()
+    message.channel.send(voteEmbed)                
+
+    }
+}
