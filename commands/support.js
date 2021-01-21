719549533f0aae18de1bed967447bdb9c6568ad0
Fefe <F33f11f00fum@gmail.com>
2021-01-21 12:16:24 +0000

    added emote cuz epic

--- commands/support.js
+++ commands/support.js
@@ -5,10 +5,11 @@ module.exports = {
     aliases: ['support', 'error','bugs'],
     execute(message, args){
       const Discord = require('discord.js');
+      const Bug_Capture = client.emojis.cache.get('801786570645962753')
       const supportembed = new Discord.MessageEmbed()
             .setColor('#FF5733')
             .setTitle("Want Some Help?")
-            .setDescription("**If You Want Some Support Join [This Server](https://discord.gg/TJUFyfdc9b)**")
+            .setDescription(`**If You Want Some Support Or Spot A Bug Join [This Server](https://discord.gg/TJUFyfdc9b)** ${Bug_Capture}`)
     message.channel.send(supportembed)
   }
 }
