2af8d38589355094ff11c90373188d9c5c30606c
Fefe <F33f11f00fum@gmail.com>
2021-02-01 11:23:00 +0000

    added a module i missed out

--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,5 @@
-const { MessageEmbed, Discord } = require("discord.js");
-const human = require("humanize");
-const paginationEmbed = require("discord.js-pagination");
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
 const https = require('https');
 module.exports = {
   name: "meme",
@@ -14,14 +13,14 @@ module.exports = {
       });
       resp.on('end', () => {
           var res = JSON.parse(data);
-          const embed = new Discord.MessageEmbed()
+          const MemeEmbed = new Discord.MessageEmbed()
               .setColor(message.member.displayColor)
               .setTitle(res.title)
               .setURL(res.permalink)
               .setImage(res.image)
               .setTimestamp()
               .setFooter(`Requested by ${message.author.username}`)
-          message.channel.send(embed);
+          message.channel.send(MemeEmbed);
       });
   });
   }
