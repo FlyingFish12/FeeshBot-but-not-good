cd246737d0e0001ab402f12f6284f4c1aed27e0b
Fefe <F33f11f00fum@gmail.com>
2021-02-05 08:37:19 +0000

    added error message if it crashes

--- commands/kick.js
+++ commands/kick.js
@@ -3,29 +3,38 @@ const Discord = require("discord.js");
 const human = require("humanize");
 const paginationEmbed = require("discord.js-pagination");
 module.exports = {
-  name: "kick",
-  description: "A kick command",
-  aliases: ["k"],
-  execute(message, args, client) {
-    if (!message.member.hasPermission('KICK_MEMBERS'))
-        return message.channel.send("⚠️ Insufficient permissions")
-        const member = message.mentions.members.first();
-    if (!member)
-        return message.channel.send("⚠️ No user mentioned.")
-        const reason = args.slice(2).join(" ") 
-    if (!member.kickable)
-        return message.channel.send("⚠️ I cannot kick this user.")
-    if (member) {
-        if (!reason) {
-            return member.kick().then(member => {
-            message.channel.send(`${member.user.tag} was kicked by ${message.author}. no reason was provided.`);
-        })
+    name: "kick",
+    description: "A kick command",
+    aliases: ["k"],
+    execute(message, args, client) {
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+          return message.channel.send("⚠️ Insufficient permissions")
+          const member = message.mentions.members.first();
+      if (!member)
+          return message.channel.send("⚠️ No user mentioned.")
+          const reason = args.slice(2).join(" ") 
+      if (!member.kickable)
+          return message.channel.send("⚠️ I cannot kick this user.")
+      if (member === message.author)
+          return message.channel.send("⚠️ You can't kick yourself!")
+      if (member === client)
+          return message.channel.send("Don't kick me ;-;")
+          try {
+      if (member) {
+          if (!reason) {
+              return member.kick().then(member => {
+              message.channel.send(`${member.user.tag} was kicked by ${message.author}. no reason was provided.`);
+          })
+      }
+      if (reason) {
+          member.kick().then(member => {
+          message.channel.send(`${member.user.tag} was kicked by ${message.author}. ${reason}.`);
+      })
+      }
+     }
+      } catch (err) {
+      console.log(err)
+      return message.channel.send('⚠️ Failed to kick. Maybe I don\'t have the correct permissions? Please Try again.', err.message);
     }
-    if (reason) {
-        member.kick().then(member => {
-        message.channel.send(`${member.user.tag} was kicked by ${message.author}. ${reason}.`);
-    })
    }
   }
- }
-}
