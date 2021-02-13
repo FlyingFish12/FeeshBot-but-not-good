5cf4f13db1f23ffa923a1a821aba7e093636ef4d
Fefe <F33f11f00fum@gmail.com>
2021-02-13 17:45:16 +0000

    hate this

--- commands/say.js
+++ commands/say.js
@@ -6,13 +6,12 @@ module.exports ={
     name: 'say',
     description: "A Say Command",
     aliases: ['say','s','write', 'repeat'],
-    execute(message,client){
-    let args = message.content.split(" ").slice(1);
-    message.delete();
+    execute(message, args, client){
+    let saymessage = args.slice(1).join(" ")
 
         const sayembed = new Discord.MessageEmbed()
             .setColor('#50C878')
-            .setDescription(args.join(` `)).cleanContent;
+            .setDescription(saymessage).cleanContent;
         message.channel.send(sayembed)
     }
 }
