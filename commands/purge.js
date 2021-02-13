64f7fb25fa08f090cf92d716aea3c12288cee47b
Fefe <F33f11f00fum@gmail.com>
2021-02-13 17:35:23 +0000

    added this thing that i forgot about

---
+++ commands/purge.js
@@ -0,0 +1,27 @@
+const { MessageEmbed, Discord } = require("discord.js");
+const human = require("humanize");
+const paginationEmbed = require("discord.js-pagination");
+module.exports = {
+  name: "purge",
+  description: "Deletes up to 100 messages",
+  aliases: ["p", "pur", "clear"],
+  execute(message, args, client) {
+    const messageArray = message.content.split(' ');
+    const args = messageArray.slice(1);
+    
+      if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('Lack of Perms!');
+      
+      let deleteAmount;
+    
+      if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
+    
+      if (parseInt(args[0]) > 100) {
+          return message.reply('You can only delete 100 messages at a time!')
+      } else {
+          deleteAmount = parseInt(args[0]);
+      }
+    
+      message.channel.bulkDelete(deleteAmount + 1, true);
+      message.reply(`**Successfully** Deleted ***${deleteAmount}*** Messages.`)
+    },
+};
