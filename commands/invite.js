4902bbd4e8357df00bae118ba3c21ed2d6e1e10b
Fefe <F33f11f00fum@gmail.com>
2021-02-21 18:35:21 +0000

    man i hate auths lol

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,7 @@ module.exports={
         const InviteEmbed = new Discord.MessageEmbed()
         .setColor(`#DA005A`)
         .setTitle("Invite me to your server today!")
-        .setDescription("[This Link Here](https://discord.com/api/oauth2/authorize?client_id=735918313921708053&permissions=388342&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D735918313921708053%26permissions%3D1436918%26redirect_uri%3Dhttps%253A%252F%252Fdiscord.com%252Fapi%252Foauth2%252Fauthorize%253Fclient_id%253D&response_type=code&scope=bot%20guilds%20applications.commands)")
+        .setDescription("[This Link Here](https://discord.com/oauth2/authorize?client_id=735918313921708053&permissions=268823799&scope=bot)")
         .setFooter("Everyone needs a little Feesh in their lives")
         .setTimestamp()
 message.channel.send(InviteEmbed)                
