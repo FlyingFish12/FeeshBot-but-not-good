ec922d3cd4814f15670daa56697b9617f4d78921
Fefe <65780441+FlyingFish12@users.noreply.github.com>
2021-04-06 16:50:09 +0100

    E

--- commands/announcement.js
+++ commands/announcement.js
@@ -8,7 +8,7 @@ module.exports = {
   aliases: ["announce"],
   execute: async(message, args, client) => {
    if(!message.guild.id === 787339571422691368) return;
-const announce = message.guild.roles.cache.find(aR => aR.id === "828348238753955870")
+const announce = message.guild.roles.cache.find(aR => aR.name === "Announcements")
 const Target = message.author
 
 try {
@@ -17,6 +17,7 @@ await Target.roles.add(announce);
 
 message.channel.send('successfully added the announcement role');
 } catch (err) {
+console.log(err)
 message.channel.send('Failed to add the role!')
     }
   }
